e8570cdcd41bd94d5df5d69c49249a5f55547fe6
Larshoog <larshoogland1@gmail.com>
2024-01-02 22:23:29 +0100

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,12 @@ import Link from 'next/link'
 import groq from 'groq'
 import client from '../client'
 
-const Index = ({posts}:any) => {
+const Index = ({posts}: any) => {
     return (
         <div>
             <h1>Cathelijne de Man</h1>
             {posts.length > 0 && posts.map(
-                ({ _id, title = '', slug = '', publishedAt = '' }) =>
+                ({_id, title = '', slug = '', publishedAt = ''}) =>
                     slug && (
                         <li key={_id}>
                             <Link href={`/post/${encodeURIComponent(slug.current)}`}>
