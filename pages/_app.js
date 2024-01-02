a260c6014e09d72b63a7394c606b8e4c1f6c00dd
Larshoog <larshoogland1@gmail.com>
2024-01-02 22:48:58 +0100

    remove typescript

---
+++ pages/_app.js
@@ -0,0 +1,25 @@
+import { createTheme, ThemeProvider } from '@mui/material'
+import React from 'react'
+import '../styles/global.css'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#EA9E8D',
+    },
+    secondary: {
+      main: '#3ABEFF',
+    },
+    tertiary: {
+      main: '#26FFE6',
+    },
+  },
+})
+
+const App = ({ Component, pageProps }) => (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+)
+
+export default App
