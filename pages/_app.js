6265bc8838ee47c05b62dcab543c889b13de9559
Larshoog <larshoogland1@gmail.com>
2024-01-09 16:43:10 +0100

    Color test blue pink

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import '../styles/global.css'
 const theme = createTheme({
     palette: {
         primary: {
-            main: '#F7BAFF',
+            main: '#EA9E8D',
         },
         secondary: {
             main: '#3ABEFF',
