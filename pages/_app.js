cdb73bee591ba56dfa3fe44aed9957c9521c6721
Larshoog <larshoogland1@gmail.com>
2024-02-05 18:07:06 +0100

    Add analytics v2

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import {createTheme, ThemeProvider} from '@mui/material'
 import React from 'react'
 import '../styles/global.css'
 import Footer from "../components/Footer";
+import { Analytics } from '@vercel/analytics/react';
 
 const theme = createTheme({
     palette: {
@@ -24,6 +25,7 @@ const App = ({Component, pageProps}) => (
     <ThemeProvider theme={theme}>
         <main>
             <Component {...pageProps} />
+            <Analytics/>
         </main>
         <Footer/>
     </ThemeProvider>
